5045253af0889196f40d6cdccbbe059e81abbc35
Antony dos santos felisberto <88542984+AntonyFelisberto@users.noreply.github.com>
2023-12-14 14:46:43 -0300

    criando modelos do projeto

---
+++ microservices/src/main/java/com/projeto/microservices/services/impl/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package com.projeto.microservices.services.impl;
+
+import com.projeto.microservices.domain.User;
+import com.projeto.microservices.repositories.UserRepository;
+import com.projeto.microservices.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository repository;
+
+    @Override
+    public User findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+}
